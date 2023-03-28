c08cdfd798bfb57af84471e08b3719c5cd9429f6
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-03-28 14:42:59 +0100

    filter by platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import { Genre } from './hooks/useGenres'
+import { Platform } from './hooks/useGames'
 import Navbar from './components/Navbar'
 import GameGride from './components/GameGride'
 import GenreList from './components/GenreList'
-import { useState } from 'react'
-import { Genre } from './hooks/useGenres'
+import PlatformSelector from './components/PlatformSelector'
 
 function App() {
   const [selectedGenre, setSelectedGenere] = useState<Genre | null>(null)
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
 
   return (
     <Grid
@@ -28,7 +31,8 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <GameGride selectedGenre={selectedGenre} />
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)} />
+        <GameGride selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   )
--- src/components/GameGride.tsx
+++ src/components/GameGride.tsx
@@ -1,16 +1,18 @@
-import { Text, SimpleGrid } from '@chakra-ui/react'
-import useGames from '../hooks/useGames'
-import GameCard from './GameCard'
-import GameCardSkeleton from './GameCardSkeleton'
-import GameCardContainer from './GameCardContainer'
-import { Genre } from '../hooks/useGenres'
+import { Text, SimpleGrid } from '@chakra-ui/react';
+import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
+import { Genre } from '../hooks/useGenres';
+import {Platform} from '../hooks/useGames';
 
 interface Props {
-  selectedGenre: Genre | null
+  selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
 
-const GameGride = ({ selectedGenre }: Props) => {
-  const { data, error, loading } = useGames(selectedGenre)
+const GameGride = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, loading } = useGames(selectedGenre, selectedPlatform)
   const skeletons = [1, 2, 3, 4, 5, 6]
   return (
     <>
