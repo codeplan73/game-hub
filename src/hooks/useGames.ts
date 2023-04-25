27a160765bbc15ca998202f277b0b12e1200748d
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-04-25 11:32:43 +0100

    refactoring - creating reusable api-client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
-import apiClient from '../services/api-client'
 import { GameQuery } from '../App'
-import { FetchResponse } from './../services/api-client'
+import APIClient,{ FetchResponse } from './../services/api-client'
 import {Platform} from './usePlatforms'
 
 export interface Game {
@@ -13,17 +12,19 @@ export interface Game {
   rating_top: number;
 }
 
+const apiClient = new APIClient<Game>('/games')
+
 const useGames = (gameQuery: GameQuery) =>
   useQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () => apiClient.get<FetchResponse<Game>>('/games', {
+    queryFn: () => apiClient.getAll({
       params: {
         genres: gameQuery.genre?.id,
         parent_platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText,
       },
-    }).then(res => res.data)
+    })
   })
 
 export default useGames
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,29 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 export interface FetchResponse<T> {
   count: number
   results: T[]
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: '8d5db4c7669440f896b688c23591dce8',
   },
 })
+
+class APIClient<T> {
+  endpoint: string
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint
+  }
+
+  getAll =(config: AxiosRequestConfig) =>{
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data)
+  }
+}
+
+export default APIClient;
